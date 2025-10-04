7e2044ee559aa5c6cb4e8ed450470bff7730651d
kasper <kasper_vegte@hotmail.com>
2025-10-04 20:45:39 +0200

    created repro

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,22 +1,107 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import {
+  Collection,
+  Entity,
+  ManyToMany,
+  ManyToOne,
+  MikroORM,
+  PrimaryKeyProp,
+  Property,
+  Ref,
+  Reference,
+} from '@mikro-orm/sqlite';
+import { v4 as uuid } from 'uuid';
 
-@Entity()
-class User {
+@Entity({ tableName: 'tenants' })
+class TenantEntity {
+  @Property({ type: 'uuid', primary: true })
+  id: string = uuid();
 
-  @PrimaryKey()
-  id!: number;
+  @Property({ type: 'string' })
+  name!: string;
+}
 
-  @Property()
-  name: string;
+@Entity({ tableName: 'auth_roles' })
+class AuthRoleEntity {
+  @Property({ type: 'uuid', primary: true })
+  id: string = uuid();
 
-  @Property({ unique: true })
-  email: string;
+  @ManyToOne({
+    entity: () => TenantEntity,
+    fieldName: 'tenant_id',
+    primary: true,
+  })
+  tenant!: Ref<TenantEntity>;
 
-  constructor(name: string, email: string) {
-    this.name = name;
-    this.email = email;
+  @Property({ type: 'string' })
+  name!: string;
+
+  @ManyToMany({ entity: () => UserProfileEntity, mappedBy: 'roles' })
+  userProfiles = new Collection<UserProfileEntity>(this);
+}
+
+@Entity({ tableName: 'user_profiles' })
+class UserProfileEntity {
+  @Property({ type: 'uuid', primary: true })
+  id: string = uuid();
+
+  @ManyToOne({
+    entity: () => TenantEntity,
+    fieldName: 'tenant_id',
+    primary: true,
+  })
+  tenant!: Ref<TenantEntity>;
+
+  @Property({ type: 'string' })
+  name!: string;
+
+  @ManyToMany({
+    entity: () => AuthRoleEntity,
+    pivotEntity: () => AuthUserRoleEntity,
+    owner: true,
+  })
+  roles = new Collection<AuthRoleEntity>(this);
+}
+
+@Entity({ tableName: 'auth_user_roles' })
+class AuthUserRoleEntity {
+  [PrimaryKeyProp]?: ['userProfile', 'role'];
+
+  @ManyToOne({
+    entity: () => UserProfileEntity,
+    primary: true,
+    updateRule: 'no action',
+    deleteRule: 'no action',
+    joinColumns: ['user_profile_id', 'tenant_id'],
+    referencedColumnNames: ['id', 'tenant_id'],
+    ref: true,
+  })
+  userProfile!: Ref<UserProfileEntity>;
+
+  @ManyToOne({
+    entity: () => AuthRoleEntity,
+    primary: true,
+    updateRule: 'no action',
+    deleteRule: 'no action',
+    joinColumns: ['role_id', 'tenant_id'],
+    referencedColumnNames: ['id', 'tenant_id'],
+    ref: true,
+  })
+  role!: Ref<AuthRoleEntity>;
+
+  @ManyToOne({
+    entity: () => TenantEntity,
+    fieldName: 'tenant_id',
+    primary: true,
+  })
+  tenant!: Ref<TenantEntity>;
+
+  @Property({ type: 'timestamp', defaultRaw: 'CURRENT_TIMESTAMP' })
+  createdAt!: Date;
+
+  constructor(userProfile: UserProfileEntity, role: AuthRoleEntity) {
+    this.userProfile = Reference.create(userProfile);
+    this.role = Reference.create(role);
   }
-
 }
 
 let orm: MikroORM;
@@ -24,7 +109,12 @@ let orm: MikroORM;
 beforeAll(async () => {
   orm = await MikroORM.init({
     dbName: ':memory:',
-    entities: [User],
+    entities: [
+      TenantEntity,
+      AuthRoleEntity,
+      UserProfileEntity,
+      AuthUserRoleEntity,
+    ],
     debug: ['query', 'query-params'],
     allowGlobalContext: true, // only for testing
   });
@@ -36,16 +126,19 @@ afterAll(async () => {
 });
 
 test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
-  await orm.em.flush();
-  orm.em.clear();
-
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
+  const tenant = orm.em.create(TenantEntity, { name: 'Tenant 1' });
   await orm.em.flush();
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+  const userProfile = orm.em.create(UserProfileEntity, { name: 'John Doe', tenant });
+  const role = orm.em.create(AuthRoleEntity, { name: 'Admin', tenant });
+  await orm.em.flush();
+
+  const userRole = new AuthUserRoleEntity(userProfile, role)
+  userRole.tenant = Reference.create(tenant)
+
+  orm.em.create(AuthUserRoleEntity, userRole);
+  await orm.em.flush();
+
+  const user = await orm.em.findOneOrFail(UserProfileEntity, { name: 'John Doe' }, {populate: ['roles']});
+  expect(user.roles[0].name).toBe('Admin');
 });
